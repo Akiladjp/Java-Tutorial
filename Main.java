454032e4b77b2cc04b643cf9ea69ef3bbdf0b05c
Akila Piyumal <92683626+Akiladjp@users.noreply.github.com>
2024-08-06 13:45:33 +0530

    First

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String [] args) {
+        Scanner x = new Scanner(System.in);
+
+        String name = x.nextline();
+        System.out.println(name);
+    }
+}
